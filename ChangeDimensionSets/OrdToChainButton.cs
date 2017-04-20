c15cf74a177c30f9c28c9548b5609c81f165519c
Ben Winding <ben.winding@gmail.com>
2017-04-21 01:50:10 +0930

    Implemented OrdToChain

--- ChangeDimensionSets/OrdToChainButton.cs
+++ ChangeDimensionSets/OrdToChainButton.cs
@@ -11,73 +11,117 @@ namespace InvAddIn
     {
         #region "Methods"
 
-        public OrdToChainButton(string displayName, string internalName, CommandTypesEnum commandType, string clientId, string description, string tooltip, Icon standardIcon, Icon largeIcon, ButtonDisplayEnum buttonDisplayType)
-            : base(displayName, internalName, commandType, clientId, description, tooltip, standardIcon, largeIcon, buttonDisplayType)
+        public OrdToChainButton(string displayName, string internalName, CommandTypesEnum commandType, string clientId,
+            string description, string tooltip, Icon standardIcon, Icon largeIcon, ButtonDisplayEnum buttonDisplayType)
+            : base(
+                displayName, internalName, commandType, clientId, description, tooltip, standardIcon, largeIcon,
+                buttonDisplayType)
         {
 
         }
 
         protected override void ButtonDefinition_OnExecute(NameValueMap context)
         {
-            //check to make sure a sketch is active
+            ConvertOrdinateSetToChainSet();
+        }
+
+        private void ConvertOrdinateSetToChainSet()
+        {
             var drawing = InventorApplication.ActiveDocument as DrawingDocument;
             if (drawing == null)
                 return;
-            if (drawing.SelectSet.Count > 0)
+            if (drawing.SelectSet.Count == 0)
                 return;
-            if (drawing.SelectSet.OfType<ChainDimensionSet>() > 0)
+            var selectedLinearInChainSet = drawing.SelectSet
+                .OfType<OrdinateDimension>()
+                .Where(d => d.IsOrdinateSetMember)
+                .ToList();
+            if (!selectedLinearInChainSet.Any())
                 return;
-            //if same session, combobox definitions will already exist
-            ComboBoxDefinition slotWidthComboBoxDefinition;
-            slotWidthComboBoxDefinition = (Inventor.ComboBoxDefinition)InventorApplication.CommandManager.ControlDefinitions["Autodesk:SimpleAddIn:SlotWidthCboBox"];
 
-            ComboBoxDefinition slotHeightComboBoxDefinition;
-            slotHeightComboBoxDefinition = (Inventor.ComboBoxDefinition)InventorApplication.CommandManager.ControlDefinitions["Autodesk:SimpleAddIn:SlotHeightCboBox"];
-
-            //get the selected width from combo box
-            double slotWidth;
-            slotWidth = slotWidthComboBoxDefinition.ListIndex;
-
-            //get the selected height from combo box
-            double slotHeight;
-            slotHeight = slotHeightComboBoxDefinition.ListIndex;
-
-            if (slotWidth > 0 && slotHeight > 0)
+            var ordinateDimensionSets = selectedLinearInChainSet
+                .Select(x => x.OrdinateDimensionSet)
+                .Distinct()
+                .ToList();
+            var ordinateCount = ordinateDimensionSets.Count;
+            for (var i=0; i<ordinateCount; i++)
             {
-                //draw the sketch for the slot
-                PlanarSketch planarSketch;
-                planarSketch = (PlanarSketch)InventorApplication.ActiveEditObject;
-
-                SketchLine[] lines = new SketchLine[2];
-                SketchArc[] arcs = new SketchArc[2];
-
-                TransientGeometry transientGeometry;
-                transientGeometry = InventorApplication.TransientGeometry;
-
-                //start a transaction so the slot will be within a single undo step
-                Transaction createSlotTransaction;
-                createSlotTransaction = InventorApplication.TransactionManager.StartTransaction(InventorApplication.ActiveDocument, "Create Slot");
-
-                //draw the lines and arcs that make up the shape of the slot
-                lines[0] = planarSketch.SketchLines.AddByTwoPoints(transientGeometry.CreatePoint2d(0, 0), transientGeometry.CreatePoint2d(slotWidth, 0));
-                arcs[0] = planarSketch.SketchArcs.AddByCenterStartEndPoint(transientGeometry.CreatePoint2d(slotWidth, slotHeight / 2.0), lines[0].EndSketchPoint, transientGeometry.CreatePoint2d(slotWidth, slotHeight), true);
-
-                lines[1] = planarSketch.SketchLines.AddByTwoPoints(arcs[0].EndSketchPoint, transientGeometry.CreatePoint2d(0, slotHeight));
-                arcs[1] = planarSketch.SketchArcs.AddByCenterStartEndPoint(transientGeometry.CreatePoint2d(0, slotHeight / 2.0), lines[1].EndSketchPoint, lines[0].StartSketchPoint, true);
-
-                //create the tangent constraints between the lines and arcs
-                planarSketch.GeometricConstraints.AddTangent((SketchEntity)lines[0], (SketchEntity)arcs[0], null);
-                planarSketch.GeometricConstraints.AddTangent((SketchEntity)lines[1], (SketchEntity)arcs[0], null);
-                planarSketch.GeometricConstraints.AddTangent((SketchEntity)lines[1], (SketchEntity)arcs[1], null);
-                planarSketch.GeometricConstraints.AddTangent((SketchEntity)lines[0], (SketchEntity)arcs[1], null);
-
-                //create a parallel constraint between the two lines
-                planarSketch.GeometricConstraints.AddParallel((SketchEntity)lines[0], (SketchEntity)lines[1], true, true);
-
-                //end the transaction
-                createSlotTransaction.End();
+                var ordinateDimensionSet = ordinateDimensionSets[i];
+                ProccessOrdinateSet(drawing.ActiveSheet, ordinateDimensionSet);
+                ordinateDimensionSet.Delete();
             }
-
-            #endregion
         }
+
+        private void ProccessOrdinateSet(Sheet drawingActiveSheet, OrdinateDimensionSet ordinateSet)
+        {
+            var intents = GetIntentsFromOrdinatesSet(ordinateSet);
+            var placementPoint = (ordinateSet.Members[1] as OrdinateDimension).Text.Origin;
+            var dimensionType = ordinateSet.DimensionType;
+
+            drawingActiveSheet.DrawingDimensions.ChainDimensionSets.Add(intents, placementPoint, dimensionType);
+        }
+
+        private void ConvertChainSetToOrdinateSet()
+        {
+            var drawing = InventorApplication.ActiveDocument as DrawingDocument;
+            if (drawing == null)
+                return;
+            if (drawing.SelectSet.Count == 0)
+                return;
+            var selectedLinearInChainSet = drawing.SelectSet
+                .OfType<LinearGeneralDimension>()
+                .Where(d => d.IsChainSetMember)
+                .ToList();
+            if (!selectedLinearInChainSet.Any())
+                return;
+
+            var chainSets = selectedLinearInChainSet
+                .Select(x => x.ChainDimensionSet)
+                .Distinct();
+
+            foreach (var chainSet in chainSets)
+            {
+                ProccessChainSet(drawing.ActiveSheet, chainSet);
+            }
+        }
+
+        private void ProccessChainSet(Sheet drawingActiveSheet, ChainDimensionSet chainSet)
+        {
+            var intents = GetIntentsFromChainSet(chainSet);
+            var placementPoint = (chainSet.Members[1] as LinearGeneralDimension).Text.Origin;
+            var dimensionType = chainSet.DimensionType;
+
+            drawingActiveSheet.DrawingDimensions.OrdinateDimensionSets.Add(intents, placementPoint, dimensionType);
+        }
+
+        private ObjectCollection GetIntentsFromChainSet(ChainDimensionSet chainSet)
+        {
+            var memberCount = chainSet.Members.Count;
+            var collection = InventorApplication.TransientObjects.CreateObjectCollection();
+            for (int i = 1; i <= memberCount; i++)
+            {
+                var intent = (chainSet.Members[i] as LinearGeneralDimension).IntentOne;
+                collection.Add(intent);
+            }
+            var intentLast = (chainSet.Members[memberCount] as LinearGeneralDimension).IntentTwo;
+            collection.Add(intentLast);
+            return collection;
+        }
+
+        private ObjectCollection GetIntentsFromOrdinatesSet(OrdinateDimensionSet chainSet)
+        {
+            var memberCount = chainSet.Members.Count;
+            var collection = InventorApplication.TransientObjects.CreateObjectCollection();
+            for (int i = 1; i <= memberCount; i++)
+            {
+                var intent = (chainSet.Members[i] as OrdinateDimension).Intent;
+                collection.Add(intent);
+            }
+//            var intentLast = (chainSet.Members[memberCount] as OrdinateDimension).Intent;
+  //          collection.Add(intentLast);
+            return collection;
+        }
+
+        #endregion
     }
+}
--- ChangeDimensionSets/test1.csx
+++ ChangeDimensionSets/test1.csx
@@ -1,8 +1,27 @@
 #r "ReferenceDDLs\Autodesk.Inventor.Interop.dll"
 using Inventor;
 using System.Runtime.InteropServices;
+using System.ComponentModel;
+
 const string ProgId = "Inventor.Application";
 Inventor.Application app = (Inventor.Application) Marshal.GetActiveObject(ProgId);
 
+private void WriteProps(object obj)
+{
+    Console.WriteLine(" ----- Getting Properties ----- ");
+	foreach(PropertyDescriptor descriptor in TypeDescriptor.GetProperties(obj))
+	{
+	    string name=descriptor.Name;
+	    object value=descriptor.GetValue(obj);
+	    Console.WriteLine("{0}={1}",name,value);
+	}
+}
+
 var doc = app.ActiveDocument as DrawingDocument;
 var selectSet = doc.SelectSet;
+object first = selectSet[1];
+WriteProps(first);
+var ordinate = (first as OrdinateDimension);
+WriteProps(ordinate.OrdinateDimensionSet);
+var ordinateSet = (ordinate.OrdinateDimensionSet as OrdinateDimension);
+WriteProps(ordinateSet.Members[1]);
